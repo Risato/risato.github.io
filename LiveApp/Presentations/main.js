5a495cde47dc73699e77a3dd9651c25cd870ad8b
Risato <rickypsatomi@gmail.com>
2025-04-19 00:27:19 -0700

    Update Map

--- LiveApp/Presentations/main.js
+++ LiveApp/Presentations/main.js
@@ -8,8 +8,6 @@
 // PASTE YOUR URLs HERE
 // these URLs come from Google Sheets 'shareable link' form
 // the first is the geometry layer and the second the points
-let geomURL =
-  "https://docs.google.com/spreadsheets/d/e/2PACX-1vTsAyA0Hpk_-WpKyN1dfqi5IPEIC3rqEiL-uwElxJpw_U7BYntc8sDw-8sWsL87JCDU4lVg2aNi65ES/pub?output=csv";
 let pointsURL =
   "https://docs.google.com/spreadsheets/d/e/2PACX-1vROZsz1MpAT-9wbZ37GrFf5Ueptndch2N7ceBJBfCC0vFO2cukgcUzWZ7zsMo6yApHkqvEw-iNDtuyM/pub?gid=1375559982&single=true&output=csv";
 
@@ -59,77 +57,11 @@ function init() {
 
   // Use PapaParse to load data from Google Sheets
   // And call the respective functions to add those to the map.
-  Papa.parse(geomURL, {
-    download: true,
-    header: true,
-    complete: addGeoms,
-  });
   Papa.parse(pointsURL, {
     download: true,
     header: true,
     complete: addPoints,
   });
-}
-
-/*
- * Expects a JSON representation of the table with properties columns
- * and a 'geometry' column that can be parsed by parseGeom()
- */
-function addGeoms(data) {
-  data = data.data;
-  // Need to convert the PapaParse JSON into a GeoJSON
-  // Start with an empty GeoJSON of type FeatureCollection
-  // All the rows will be inserted into a single GeoJSON
-  let fc = {
-    type: "FeatureCollection",
-    features: [],
-  };
-
-  for (let row in data) {
-    // The Sheets data has a column 'include' that specifies if that row should be mapped
-    if (data[row].include == "y") {
-      let features = parseGeom(JSON.parse(data[row].geometry));
-      features.forEach((el) => {
-        el.properties = {
-          name: data[row].name,
-          description: data[row].description,
-        };
-        fc.features.push(el);
-      });
-    }
-  }
-
-  // The geometries are styled slightly differently on mouse hovers
-  let geomStyle = { color: "#2ca25f", fillColor: "#99d8c9", weight: 2 };
-  let geomHoverStyle = { color: "green", fillColor: "#2ca25f", weight: 3 };
-
-  L.geoJSON(fc, {
-    onEachFeature: function (feature, layer) {
-      layer.on({
-        mouseout: function (e) {
-          e.target.setStyle(geomStyle);
-        },
-        mouseover: function (e) {
-          e.target.setStyle(geomHoverStyle);
-        },
-        click: function (e) {
-          // This zooms the map to the clicked geometry
-          // Uncomment to enable
-          // map.fitBounds(e.target.getBounds());
-
-          // if this isn't added, then map.click is also fired!
-          L.DomEvent.stopPropagation(e);
-
-          document.getElementById("sidebar-title").innerHTML =
-            e.target.feature.properties.name;
-          document.getElementById("sidebar-content").innerHTML =
-            e.target.feature.properties.description;
-          sidebar.open(panelID);
-        },
-      });
-    },
-    style: geomStyle,
-  }).addTo(map);
 }
 
 /*
@@ -167,25 +99,25 @@ function addPoints(data) {
     marker.addTo(pointGroupLayer);
 
     // UNCOMMENT THIS LINE TO USE POPUPS
-    //marker.bindPopup('<h2>' + data[row].name + '</h2>There's a ' + data[row].description + ' here');
+    marker.bindPopup('<h2>' + data[row].name + '</h2>There's a ' + data[row].description + ' here');
 
     // COMMENT THE NEXT GROUP OF LINES TO DISABLE SIDEBAR FOR THE MARKERS
-    marker.feature = {
-      properties: {
-        name: data[row].name,
-        description: data[row].Start + data[row].location,
-      },
-    };
-    marker.on({
-      click: function (e) {
-        L.DomEvent.stopPropagation(e);
-        document.getElementById("sidebar-title").innerHTML =
-          e.target.feature.properties.name;
-        document.getElementById("sidebar-content").innerHTML =
-          e.target.feature.properties.description;
-        sidebar.open(panelID);
-      },
-    });
+    // marker.feature = {
+    //   properties: {
+    //     name: data[row].name,
+    //     description: data[row].Start + data[row].location,
+    //   },
+    // };
+    // marker.on({
+    //   click: function (e) {
+    //     L.DomEvent.stopPropagation(e);
+    //     document.getElementById("sidebar-title").innerHTML =
+    //       e.target.feature.properties.name;
+    //     document.getElementById("sidebar-content").innerHTML =
+    //       e.target.feature.properties.description;
+    //     sidebar.open(panelID);
+    //   },
+    // });
     // COMMENT UNTIL HERE TO DISABLE SIDEBAR FOR THE MARKERS
 
     // AwesomeMarkers is used to create fancier icons
@@ -199,42 +131,5 @@ function addPoints(data) {
     if (!markerType.includes("circle")) {
       marker.setIcon(icon);
     }
-  }
-}
-
-/*
- * Accepts any GeoJSON-ish object and returns an Array of
- * GeoJSON Features. Attempts to guess the geometry type
- * when a bare coordinates Array is supplied.
- */
-function parseGeom(gj) {
-  // FeatureCollection
-  if (gj.type == "FeatureCollection") {
-    return gj.features;
-  }
-
-  // Feature
-  else if (gj.type == "Feature") {
-    return [gj];
-  }
-
-  // Geometry
-  else if ("type" in gj) {
-    return [{ type: "Feature", geometry: gj }];
-  }
-
-  // Coordinates
-  else {
-    let type;
-    if (typeof gj[0] == "number") {
-      type = "Point";
-    } else if (typeof gj[0][0] == "number") {
-      type = "LineString";
-    } else if (typeof gj[0][0][0] == "number") {
-      type = "Polygon";
-    } else {
-      type = "MultiPolygon";
-    }
-    return [{ type: "Feature", geometry: { type: type, coordinates: gj } }];
   }
 }
